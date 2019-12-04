f50cbb87dd1e8bafb6ca3c1165c68f387ba984db
escobar55 <escobar5@umbc.edu>
2019-12-04 15:32:20 -0500

    sec

---
+++ src/main/java/com/example/demo/UserDeptAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDeptAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDeptAppApplication.class, args);
+    }
+
+}
